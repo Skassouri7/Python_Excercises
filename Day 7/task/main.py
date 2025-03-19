b9944c8eda65b7fb27523cd940031b8421a9c5ad
Soufiane Kassouri <Soufiane.kassouri.2021@mumail.ie>
2025-03-19 00:52:47 +0000

    Day 7 Completed

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -5,22 +5,30 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 chosenWord = []
-placeHolder = []
+placeHolder = ""
 display = []
 
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-for i in chosen_word:
-    chosenWord.append(i)
-for position in range(len(chosen_word)):
-    chosenWord.append(i)
-
+for size in range(len(chosen_word)):
+    placeHolder += "_"
 
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+for i in chosen_word:
+    chosenWord.append(i)
+    display.append("_")
+
+print(placeHolder)
+
 while display != chosenWord:
+    result = ""
     guess = input("Guess a letter: ").lower()
     for position in range(len(chosen_word)):
-            if position == guess:
-                display[position] = chosenWord[letter]
-            else:
-                display[position] = "_"
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    for i in display:
+        result += i
     print(display)
+    print(result)
+
+print("Congratulations you win !")
