295ff80225533e552b951e4c202a5ab12fe58a15
Soufiane Kassouri <Soufiane.kassouri.2021@mumail.ie>
2025-03-24 00:16:58 +0000

    Day 8 Completed

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -4,23 +4,30 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-def encrypt(original_text, shift_amount):
+def caesar(original_text, shift_amount, encodeOrDecode):
     cipher_text = ""
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+    if encodeOrDecode == 'encode':
+        for letter in original_text:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
+        print(f"Here is the encoded result: {cipher_text}")
+    elif encodeOrDecode == 'decode':
+        for letter in original_text:
+            shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
+        print(f"Here is the decoded result: {cipher_text}")
+    else:
+        print(f"Incorrect Input: {encodeOrDecode} ")
 
-
-encrypt(original_text=text, shift_amount=shift)
+caesar(text, shift, direction)
 
 
 
--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,32 +1,50 @@
 # TODO-1: Import and print the logo from art.py when the program starts.
-
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # TODO-2: What happens if the user enters a number/symbol/space?
 
-
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
 
     for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
+        if letter in alphabet:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
 
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
 # TODO-3: Can you figure out a way to restart the cipher program?
 
+wants_to_continue = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+while wants_to_continue:
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    invalid_input = True
+    while invalid_input:
+        user_input = input("Type 'yes' if you want to go again. Otherwise, type 'no'.:\n")
+        if user_input == 'no':
+            invalid_input = False
+            wants_to_continue = False
+            print("Goodbye")
+        elif user_input == 'yes':
+            wants_to_continue = True
+            invalid_input = False
+        else:
+            print("invalid input try again")
 
 
